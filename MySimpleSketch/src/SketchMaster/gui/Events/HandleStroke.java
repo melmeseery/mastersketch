bd675d27e8c252ebe16fb045181623849d46ec5c
melmeseery@gmail.com <melmeseery@gmail.com@b086b714-2e34-11df-954b-21e382534593>
2010-09-24 13:01:41 +0000

    This is saving this version into a new tag to be reterifed latter before radwas comments

--- MySimpleSketch/src/SketchMaster/gui/Events/HandleStroke.java
+++
@@ -1,9 +0,0 @@
-package SketchMaster.gui.Events;
-
-import java.util.EventListener;
-
-public interface HandleStroke extends EventListener {
-
-	public abstract void HandleNewStroke(NewStrokeEvent Evt);
-
-}
